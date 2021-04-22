1c42a1f1cff54a618d856f7e42c528f689994020
Song-jaehong <4leaf.SJH@gmail.com>
2021-04-22 15:54:14 +0900

    coinf

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import Loading from "./Loading";
 import * as Location from "expo-location";
 import axios from "axios";
 import Weather from "./Weather";
+
+import { WebView } from "react-native-webview";
 const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+
 export default class extends React.Component {
   state = {
     isLoading: true,
